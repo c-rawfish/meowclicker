a2c41494d04b2defc3fbe70611af7ab30da8664d
c-rawfish <153956454+c-rawfish@users.noreply.github.com>
2024-01-10 18:11:14 -0500

    Add files via upload

---
+++ meowclicker/clicker/meowclicker/clicker/input.cpp
@@ -0,0 +1,82 @@
+#include "input.h"
+#include "gui.h"
+
+#include <thread>
+
+namespace inputmath
+{
+	int getRandomInt(int min, int max)
+	{
+		std::random_device                  rand_dev;
+		std::mt19937                        gen(rand_dev());
+		std::uniform_int_distribution<int>  dist(min, max + 1);
+
+		return dist(gen);
+	}
+
+	float getRandomFloat(float min, float max)
+	{
+		std::random_device                  rand_dev;
+		std::mt19937                        gen(rand_dev());
+		std::uniform_real_distribution<float>  dist(min, max + 1);
+
+		return dist(gen);
+	}
+
+	float cpsToDelay(float cps)
+	{
+		return 1000 / cps;
+	}
+}
+
+namespace input
+{
+	POINT mousePos{ 0, 0 };
+	HWND foreground{ GetForegroundWindow() };
+
+	void sendClick(float blockChance)
+	{
+		GetCursorPos(&mousePos);
+		foreground = GetForegroundWindow();
+
+		LPARAM mouseParam = MAKELPARAM(mousePos.x, mousePos.y);
+		
+		PostMessageA(foreground, WM_LBUTTONDOWN, 0, mouseParam);
+		if (inputmath::getRandomFloat(0, 100) < blockChance)
+		{
+			PostMessageA(foreground, WM_RBUTTONDOWN, 0, mouseParam);
+			PostMessageA(foreground, WM_RBUTTONUP, 0, mouseParam);
+		}
+		PostMessageA(foreground, WM_LBUTTONUP, 0, mouseParam);
+	}
+
+	void sendJitter(float jitterFactor)
+	{
+		float jitterX = inputmath::getRandomFloat(-0.5F, 0.5F) * jitterFactor;
+		float jitterY = inputmath::getRandomFloat(-0.5F, 0.5F) * jitterFactor;
+
+		mousePos.x += jitterX;
+		mousePos.y += jitterY;
+
+		SetCursorPos(mousePos.x, mousePos.y);
+	}
+
+	void clickLoop()
+	{
+		while (gui::isRunning)
+		{
+			float cps = inputmath::getRandomFloat(config::minCPS, config::maxCPS + 1);
+
+			if (GetAsyncKeyState(VK_LBUTTON) && config::enabled && GetForegroundWindow() != gui::window)
+			{
+				if (config::mcWindow && GetForegroundWindow() != FindWindowA("LWJGL", nullptr))
+					continue;
+
+				input::sendClick(config::blockChance);
+				input::sendJitter(config::jitter);
+			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds((long)inputmath::cpsToDelay(cps)));
+		}
+	}
+}
---
+++ meowclicker/clicker/meowclicker/clicker/input.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <random>
+#include <Windows.h>
+
+namespace inputmath
+{
+	int getRandomInt(int min, int max);
+	float getRandomFloat(float min, float max);
+
+	float cpsToDelay(float cps);
+}
+
+namespace input
+{
+	void sendClick(float blockChance);
+	void sendJitter(float jitterFactor);
+	void clickLoop();
+}
